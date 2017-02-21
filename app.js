de2928f18a290288c413abaf1a383c83a7823fee
Erik Enderlein <erik.end@gmail.com>
2017-02-21 14:00:34 -0600

    followed along with chart making lecture

--- app.js
+++ app.js
@@ -111,3 +111,28 @@ function handleClick(event){
   print();
 };
 print();
+
+var context = document.getElementById('chart').getContext('2d');
+var data = [12,4,9,3,100,55,31];
+var labelColors = ['blue','red','orange','purple','green','yellow','salmon'];
+var chartData = {
+  type: 'bar',
+  data: {
+    labels: labelColors,
+    datasets: [{
+      label: '# of votes / color',
+      data: data,
+      backgroundColor: labelColors,
+    }],
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+};
+var myChart = new Chart(context,chartData);
