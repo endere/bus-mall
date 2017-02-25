d487e7b435c27c13fa8da6552daa41f9b7bbd7cb
Erik Enderlein <erik.end@gmail.com>
2017-02-25 13:27:41 -0600

    fixed chart so only one can display at a time, even if pressed multiple times.

--- chartdata.js
+++ chartdata.js
@@ -4,6 +4,16 @@ var dataPressed = [];
 var dataPercent = [];
 var dataLabels = [];
 var imageArray = [];
+
+function createElement(tagType, tagIdentifier, tagIdentifiername, elementContent, sectionId, tagIdentifierTwo, tagIdentifiernameTwo){
+  var element = document.createElement(tagType);
+  element.setAttribute(tagIdentifier, tagIdentifiername);
+  element.setAttribute(tagIdentifierTwo, tagIdentifiernameTwo);
+  element.textContent = elementContent;
+//  console.log(element);
+  sectionId.appendChild(element);
+  //this element creation function created by Benjamin Ayzenberg.
+}
 function returnFromStorage(){
   imageArray = localStorage.getItem('key');
   imageArray = JSON.parse(imageArray);
@@ -41,15 +51,19 @@ function createChart(dataType,tag,title){
   };
   var myChart = new Chart(context,chartData);
 }
+var canvasHolder = document.getElementById('canvasHolder');
 var buttonEl = document.getElementById('buttonForm');
 buttonEl.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event){
   event.preventDefault();
   event.stopPropagation();
+  buttonEl.removeEventListener('submit', handleSubmit);
   returnFromStorage();
   processData();
-  createChart(dataShown,'shown','# times shown');
-  createChart(dataPressed,'pressed','# times pressed');
-  createChart(dataPercent,'percent', '% of times pressed');
+  console.log(document.getElementById('shown'));
+  var shownChart = createChart(dataShown,'shown','# times shown');
+  var pressedChart = createChart(dataPressed,'pressed','# times pressed');
+  var percentChart = createChart(dataPercent,'percent', '% of times pressed');
+
 }
